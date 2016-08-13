2da7ca50dc1c2c90edf6a700c308da1a5b205567
Bob Jones <bobjinpa1@gmail.com>
2016-08-13 15:56:49 -0400

    going

--- spawner.js
+++ spawner.js
@@ -1,6 +1,10 @@
 var creeps_helper = require('creeps_helper');
 module.exports = {
     spawn: function(creeps){
-        
+       var minNumberOfHarvesters = 5;
+       var actualNumberOfHarvesters = creeps_helper.count(creeps, 'harvester');
+       if (actualNumberOfHarvesters < minNumberOfHarvesters){
+           Game.spawns.Spawn1.createCreeps([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester', working:false});
+       } 
     }
 }
