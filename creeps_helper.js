b8b046030572f94b03bf2f88b2c592b3e1d96d84
Bob Jones <bobjinpa1@gmail.com>
2016-08-14 19:17:58 -0400

    going

--- creeps_helper.js
+++ creeps_helper.js
@@ -1,9 +1,23 @@
 module.exports = {
+    killall: function () {
+        for (var i in Game.creeps) {
+            Game.creeps[i].suicide(); // lemmings style!
+        }
+    },
+    clean: function () {
+        for (var name in Memory.creeps) {
+            console.log('nope');
+            if (!Game.creeps[name]) {
+                console.log('yep');
+                delete Memory.creeps[name];
+            }
+        }
+    },
     count: function (creeps, type) {
         var counter = 0;
-        for (let index in creeps){
-            if (creeps[index].memory.role == type){
-                counter ++;
+        for (let index in creeps) {
+            if (creeps[index].memory.role == type) {
+                counter++;
             }
         }
         return counter;
