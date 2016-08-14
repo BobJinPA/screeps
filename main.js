b37d579c3281cfc22eb4682f502a0eb53c53e446
Bob Jones <bobjinpa1@gmail.com>
2016-08-14 19:13:30 -0400

    going

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var spawner = require('spawner');
 module.exports.loop = function () {
     var creeps = Game.creeps;
     //killall(creeps);
+    clean();
     runner.run(creeps);
     spawner.spawn(creeps);
 }
@@ -12,5 +13,15 @@ function killall(creeps) {
     for (var i in creeps) {
         creeps[i].suicide(); // lemmings style!
 
+    }
+}
+
+function clean(){
+    for (var name in Memory.creeps){
+        console.log('nope');
+        if(!Game.creeps[name]){
+            console.log('yep');
+            delete Memory.creeps[name];
+        }
     }
 }
